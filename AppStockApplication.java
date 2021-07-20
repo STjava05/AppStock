79af8e1efc478531a65fe6e6bb683817ec334e61
STjava05 <67196924+STjava05@users.noreply.github.com>
2021-07-20 12:47:10 +0200

    stockApplication

---
+++ AppStockApplication.java
@@ -0,0 +1,13 @@
+package it.esedra.appstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppStockApplication.class, args);
+	}
+
+}
